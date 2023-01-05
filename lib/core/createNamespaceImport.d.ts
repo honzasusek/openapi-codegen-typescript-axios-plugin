e4bac558ba3a1741116fb3c336f9f4c520934429
Honza Sušek <honza@susek.cz>
2023-01-05 09:56:48 +0100

    chore: remove build from git

--- lib/core/createNamespaceImport.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Helper to create namespace import.
- *
- * @param namespace namespace import identifier
- * @param filename path of the module
- * @returns ts.Node of the import declaration
- */
-export declare const createNamespaceImport: (namespace: string, filename: string) => import("typescript").ImportDeclaration;
--- lib/testUtils.d.ts
+++
@@ -1,5 +0,0 @@
-import ts from "typescript";
-/**
- * Print a typescript node
- */
-export declare const print: (node: ts.Node) => string;
